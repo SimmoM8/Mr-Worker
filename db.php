728a83f579a242f4e38d06c27ed960fa554a0390
SimmoM8 <benjaminsimmons@outlook.com.au>
2025-01-18 20:47:45 +0100

    database config file db.php

---
+++ db.php
@@ -0,0 +1,25 @@
+<?php
+// Load environment variables
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db = getenv('DB_NAME');
+$charset = 'utf8mb4';
+
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+  $pdo = new PDO( $dsn, $user, $pass, $options );
+} catch ( PDOException $e ) {
+
+  error_log("Database connection failed: " . $e->getMessage());
+  exit('Database connection failed.');
+
+}
+?>

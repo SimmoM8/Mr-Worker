229ede9706a7d48da0433fc8bf43fdc2e7aaceb2
SimmoM8 <benjaminsimmons@outlook.com.au>
2025-01-19 02:31:50 +0100

    removed debugging lines

--- db.php
+++ db.php
@@ -6,12 +6,6 @@ $pass = getenv('DB_PASS');
 $db = getenv('DB_NAME');
 $charset = 'utf8mb4';
 
-
-error_log(print_r($host));
-error_log(print_r($user));
-error_log(print_r($pass));
-error_log(print_r($db));
-
 $dsn = "mysql:unix_socket=/run/mysqld/mysqld.sock;dbname=$db;charset=$charset";
 $options = [
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,

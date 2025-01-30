1d4f1ca54c6b61b045c1a549041ee639b37c8113
SimmoM8 <benjaminsimmons@outlook.com.au>
2025-01-31 00:54:18 +0100

    created admin dashboard

---
+++ my/admin-dashboard.php
@@ -0,0 +1,57 @@
+<?php
+
+session_start();
+
+// If user is not logged in, return error response
+if (!isset($_SESSION['user_id'])) {
+    echo json_encode(['error' => 'User is not logged in']);
+    exit();
+}
+
+require '../db.php'; // Include the PDO connection
+
+$stmt = $pdo->prepare("SELECT count(*) FROM `users`");
+$stmt->execute();
+$userCount = $stmt->fetchColumn();
+
+$stmt = $pdo->prepare("SELECT * FROM `user_reports`");
+$stmt->execute();
+$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<section class="dashboard container-fluid">
+    <div class="background-icon"><i class="fa-solid fa-user-tie"></i></div>
+    <div class="container">
+        <h1>Admin Dashboard</h1>
+        <div class="row">
+            <div class="col-6">
+                <div class="card card-body card-colored">
+                    <h4>Users</h4>
+                    <div id="users_counter"><?php echo $userCount; ?></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col card card-body card-colored">
+                <h4>User reports and suggestions</h4>
+                <table id="user_reports_table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Report message</th>
+                            <th scope="col">Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($response as $row) {
+                            echo '<tr><th scope="row">' . $row[''] . '</th><td>' . $row['report_message'] . '</td><td>' . $row['report_date'] . '</td></tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</section>
--- my/side_bar.php
+++ my/side_bar.php
@@ -2,19 +2,16 @@
   <aside
     class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark fixed-top vh-100"
     style="width: 280px"
-    id="sidebar"
-  >
+    id="sidebar">
     <a
       href="#"
-      class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
-    >
+      class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
       <svg
         class="bi me-3"
         width="40"
         height="32"
         role="img"
-        aria-label="Bootstrap"
-      >
+        aria-label="Bootstrap">
         <use xlink:href="#bootstrap"></use>
       </svg>
       <span class="fs-4">My Dashboard</span>
@@ -22,30 +19,32 @@
     <hr />
     <ul class="sidebar-nav nav nav-pills flex-column mb-auto">
       <li class="nav-item">
-        <a class="nav-link text-white active" data-page="resumes.php"
-          ><i class="fas fa-file-lines me-3"></i>Resumes</a
-        >
+        <a class="nav-link text-white active" data-page="resumes.php"><i class="fas fa-file-lines me-3"></i>Resumes</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-white" data-page="work_experience.php"
-          ><i class="fas fa-briefcase me-3"></i>Work Experience</a
-        >
+        <a class="nav-link text-white" data-page="work_experience.php"><i class="fas fa-briefcase me-3"></i>Work Experience</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-white" data-page="education.php"
-          ><i class="fa-solid fa-graduation-cap me-3"></i>Education</a
-        >
+        <a class="nav-link text-white" data-page="education.php"><i class="fa-solid fa-graduation-cap me-3"></i>Education</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-white" data-page="skills.php"
-          ><i class="fa-solid fa-id-card me-3"></i>Skills</a
-        >
+        <a class="nav-link text-white" data-page="skills.php"><i class="fa-solid fa-id-card me-3"></i>Skills</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-white" data-page="profile.php"
-          ><i class="fa-solid fa-user-tie me-3"></i>Profile</a
+        <a class="nav-link text-white" data-page="profile.php"><i class="fa-solid fa-user-tie me-3"></i>Profile</a>
+      </li>
+      <?php
+      if ($_SESSION['user_id'] == '14') {
+        echo
+        "
+      <li class='nav-item'>
+        <a class='nav-link text-white' data-page='admin-dashboard.php'
+          ><i class='fa-solid fa-lock me-3'></i>Admin</a
         >
       </li>
+      ";
+      }
+      ?>
     </ul>
     <hr />
     <div class="dropdown">
@@ -54,8 +53,7 @@
         class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
         id="dropdownUser1"
         data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
+        aria-expanded="false">
         <div
           class="me-3"
           id="sidebarProfileContainer"
@@ -65,10 +63,9 @@
             overflow: hidden;
             border-radius: 50%;
             position: relative;
-          "
-        ></div>
+          "></div>
         <script>
-          document.addEventListener("DOMContentLoaded", function () {
+          document.addEventListener("DOMContentLoaded", function() {
             renderProfilePicture("#sidebarProfileContainer");
           });
         </script>
@@ -76,8 +73,7 @@
       </a>
       <ul
         class="dropdown-menu dropdown-menu-dark text-small shadow"
-        aria-labelledby="dropdownUser1"
-      >
+        aria-labelledby="dropdownUser1">
         <li><a class="dropdown-item" href="_details.php">New resume...</a></li>
         <li><a class="dropdown-item" href="#">Settings</a></li>
         <li><a class="dropdown-item" href="#">Profile</a></li>
